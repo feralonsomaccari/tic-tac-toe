6bee0666aed1a7cfb82b842f501316527c60cd3b
feralonsomaccari <feralonsomaccari@gmail.com>
2022-09-23 00:08:50 +0200

    Add player names input validation

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Menu.module.css";
 
 const Home = () => {
   const [player1, setPlayer1] = useState("");
   const [player2, setPlayer2] = useState("");
+
+  const [playEnabled, setPlayEnabled] = useState(false)
+
+  useEffect( () => {
+    if (player1.length && player2.length) {
+      setPlayEnabled(true);
+    } else {
+      setPlayEnabled(false);
+    }
+  }, [player1, player2])
 
   return (
     <article className={styles.menu}>
@@ -14,14 +24,14 @@ const Home = () => {
       <section className={styles.menu__container}>
         <div className={styles.menu__form}>
           <label for="player1">Player 1</label>
-          <input id="player1" onChange={(e) => setPlayer1(e)}></input>
+          <input id="player1" onChange={(e) => setPlayer1(e.target.value)}></input>
         </div>
         <div className={styles.menu__form}>
           <label for="player2">Player 2</label>
-          <input id="player2" onChange={(e) => setPlayer2(e)}></input>
+          <input id="player2" onChange={(e) => setPlayer2(e.target.value)}></input>
         </div>
 
-        <button className={`${styles.menu__button} ${styles['menu__boton--disabled']}`}>PLAY</button>
+        <button className={`${styles.menu__button} ${!playEnabled && styles['menu__boton--disabled']}`}>PLAY</button>
         <button className={styles.menu__button}>LEADERBOARD</button>
       </section>
     </article>
