6f4dbcecd42b25e405990fab502e94111b03b651
feralonsomaccari <feralonsomaccari@gmail.com>
2022-09-25 23:10:37 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import GameHistoryBoard from './Components/GameHistoryBoard/GameHistoryBoard'
-import Menu from './Components/Menu/Menu'
+import GameHistoryBoard from "./Components/GameHistoryBoard/GameHistoryBoard";
+import Menu from "./Components/Menu/Menu";
 import Game from "./Components/Game/Game";
 import { GameProvider } from "./GameContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GameProvider>
-        <GameHistoryBoard/>
-        {/* <Game player1Name="Felipe" player2Name="Pedro" /> */}
-        {/* <Menu/> */}
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Menu />} />
+            <Route path="/game" element={<Game />} />
+            <Route path="/gamehistoryboard" element={<GameHistoryBoard />} />
+          </Routes>
+        </BrowserRouter>
       </GameProvider>
     </div>
   );
