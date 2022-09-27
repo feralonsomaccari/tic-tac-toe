bd2dda6caf5d07f2374bc27112b0ac33e887eeaf
feralonsomaccari <feralonsomaccari@gmail.com>
2022-09-27 15:16:02 +0200

    Add basic leaderboard

--- src/Components/GameHistoryBoard/GameHistoryBoard.js
+++ src/Components/GameHistoryBoard/GameHistoryBoard.js
@@ -1,18 +1,45 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./GameHistoryBoard.module.css";
 import { useContext } from "react";
 import GameContext from "../../GameContext";
 
 const GameHistoryBoard = ({ setShowNav = () => "" }) => {
   const { gameHistory } = useContext(GameContext);
+  const [leaderboard, setLeaderboard] = useState([]);
+  
 
   useEffect(() => {
     setShowNav(true);
+
+    const players = {};
+    gameHistory.forEach((game) => {
+      players[game.player1] = {wins: 0};
+      players[game.player2] = {wins: 0};
+      players['TIE'] = {wins: 0};
+    });
+
+    gameHistory.forEach((game) => {
+      console.log(game)
+      players[game.winner].wins += 1;
+    });
+
+    setLeaderboard(players);
   }, []);
 
   return (
     <div className={styles.gamehistoryboard}>
-      <h1>Game History Board</h1>
+      <h1>LEADERBOARD</h1>
+      {leaderboard && (
+        Object.keys(leaderboard).map(player => {
+          return (
+            <div>
+              <span>{player}: </span>
+              <span>{leaderboard[player].wins}</span>
+            </div>
+          )
+        })
+      )}
+      <h2>Game History Board</h2>
       {gameHistory.length && (
         <table className={styles.gamehistoryboard__table}>
           <thead className={styles.gamehistoryboard__tablehead}>
